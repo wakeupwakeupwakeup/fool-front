e53a0e1b26c10cd91fb63f6e5d91964126fa52bf
Danil Morozov <dan.project1101@gmail.com>
2024-08-11 22:07:37 +0700

    fix debug

--- src/components/screens/auth/Auth.tsx
+++ src/components/screens/auth/Auth.tsx
@@ -1,48 +1,47 @@
-import { FC, useEffect, useState } from 'react'
+import { FC, useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
 
 import { Layout } from '@/components/ui'
 
-import { saveId } from '@/services/auth/auth.helper'
-
-import { useAuth } from './useAuth'
-
 const Auth: FC = () => {
-	const [id, setId] = useState('')
-	const [newId, setNewId] = useState('')
-	const { player } = useAuth(newId)
+	const location = useLocation()
 
-	const onSubmit = async () => {
-		await saveId(id)
-		setNewId(id)
-	}
+	// https://tonfool.online/menu?id=805868998&first_name=Danil&username=danil_mor&photo_url=https%3A%2F%2Ft.me%2Fi%2Fuserpic%2F320%2FktWdVGZAA3C0h0uyVs4rKxUdJ-0g4Bz0eSSk1DLEElw.jpg&auth_date=1723388640&hash=c783e9787583d6d5874ceb5d5c2de8656cec2523ea4d67cf77f741776ba42d14
 
 	useEffect(() => {
+		const queryParams = new URLSearchParams(location.search)
+
+		const id = queryParams.get('id')
+		const firstName = queryParams.get('first_name')
+		const username = queryParams.get('username')
+		const photoUrl = queryParams.get('photo_url')
+		const authDate = queryParams.get('auth_date')
+		const hash = queryParams.get('hash')
+
+		const data = {
+			id,
+			firstName,
+			username,
+			photoUrl,
+			authDate,
+			hash
+		}
+
+		console.log(data)
+
 		const script = document.createElement('script')
 		script.async = true
 		script.src = 'https://telegram.org/js/telegram-widget.js?22'
 		script.setAttribute('data-telegram-login', 'privates_manager_bot')
 		script.setAttribute('data-size', 'large')
-		script.setAttribute('data-auth-url', 'https://tonfool.online/menu')
+		script.setAttribute('data-auth-url', 'https://tonfool.online/auth')
 		script.setAttribute('data-request-access', 'write')
 
 		document.querySelector('.auth')?.appendChild(script)
 	}, [])
 
 	return (
-		<Layout className='auth flex flex-col gap-base-x3 items-center justify-center'>
-			{/*<input
-				type='text'
-				onChange={e => setId(e.target.value)}
-				className='w-full h-base-x6 border border-white rounded-base-x1 outline-0 py-base-x5 px-base-x2'
-				style={{
-					background: 'none'
-				}}
-				placeholder='TD ID'
-			/>
-			<Button className='border border-white' onClick={onSubmit}>
-				Войти
-			</Button>*/}
-		</Layout>
+		<Layout className='auth flex flex-col gap-base-x3 items-center justify-center'></Layout>
 	)
 }
 
