6415d377f17d5495f7c0aae7c4c6c395fa0f6b85
Danil Morozov <dan.project1101@gmail.com>
2024-08-11 22:22:01 +0700

    fix error

--- src/services/auth/auth.service.ts
+++ src/services/auth/auth.service.ts
@@ -3,11 +3,17 @@ import { IPlayer } from '@/shared/types/auth.interface'
 import { request } from '@/services/api/request.api'
 
 export const AuthService = {
+	async getPlayer(tg_id: string) {
+		return request<IPlayer>({
+			url: `/player/${tg_id}`,
+			method: 'GET'
+		})
+	},
 	async token(data) {
 		return request<IPlayer>({
 			url: '/token',
 			method: 'POST',
-			body: data
+			data
 		})
 	}
 }
