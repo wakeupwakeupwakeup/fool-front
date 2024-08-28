0f4241f380ed3640ec6eb58df010825d9f00d558
Danil Morozov <dan.project1101@gmail.com>
2024-08-28 23:24:58 +0700

    fix auth

--- src/components/screens/auth/Auth.tsx
+++ src/components/screens/auth/Auth.tsx
@@ -10,36 +10,13 @@ import { useTelegram } from '@/hooks'
 const Auth: FC = () => {
 	const { mutate } = useAuth()
 	const { tg } = useTelegram()
-	// const decodeURI = decodeURIComponent(tg.initDataUnsafe)
-	// const queryParams = new URLSearchParams(decodeURI)
 	const friend_id = getFriendId()
 
-	console.log(tg.initDataUnsafe)
 	useEffect(() => {
 		const data = {
-			value: tg.initDataUnsafe,
+			value: tg.initData,
 			referal_id: friend_id || null
-
-			/*user: JSON.parse(queryParams.get('user')),
-			referal_id: friend_id || null,
-			chat_instance: queryParams.get('chat_instance'),
-			chat_type: queryParams.get('chat_type'),
-			auth_date: queryParams.get('auth_date'),
-			hash: queryParams.get('hash'),
-			query_id: queryParams.get('query_id')*/
-
-			/*user: JSON.parse(queryParams.get('user')),
-			id: JSON.parse(queryParams.get('user')).id.toString(),
-			first_name: JSON.parse(queryParams.get('user')).first_name || null,
-			last_name: JSON.parse(queryParams.get('user'))?.last_name || null,
-			username: JSON.parse(queryParams.get('user'))?.username || null,
-			photo_url: JSON.parse(queryParams.get('user'))?.photo_url || null,
-			referal_id: friend_id || null,
-			auth_date: queryParams.get('auth_date'),
-			hash: queryParams.get('hash')*/
 		}
-
-		console.log(data)
 
 		if (!!data?.value) mutate(data as any)
 	}, [])
