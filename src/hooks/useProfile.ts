440fee0e030311f1db8d038154bb7608a30e3bf6
Danil Morozov <dan.project1101@gmail.com>
2024-08-29 15:25:22 +0700

    fix auth

--- src/hooks/useProfile.ts
+++ src/hooks/useProfile.ts
@@ -1,26 +1,34 @@
 import { useQuery } from '@tanstack/react-query'
 import { useAtom } from 'jotai'
 import { useMemo } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 import { useTelegram } from '@/hooks/useTelegram'
 
 import { IPlayer } from '@/shared/types/auth.interface'
 
+import { deleteId } from '@/services/auth/auth.helper'
 import { AuthService } from '@/services/auth/auth.service'
 
 import { playerAtom } from '@/store'
 
 export const useProfile = () => {
 	const { user } = useTelegram()
+	const navigate = useNavigate()
 	const [_, setPlayer] = useAtom(playerAtom)
 
 	const { data, isLoading: isUserLoading } = useQuery(
 		['getPlayer'],
 		() => AuthService.getPlayer(user.id.toString()),
 		{
-			onSuccess: (data: IPlayer) => {
-				// @ts-ignore
-				setPlayer(data)
+			onSuccess: async (data: IPlayer) => {
+				if (!data) {
+					await deleteId()
+					navigate(0)
+				} else {
+					// @ts-ignore
+					setPlayer(data)
+				}
 			}
 		}
 	)
